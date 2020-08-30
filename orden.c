b4ee9e43f937eb67fee8f60de8dda91961b48d01
LibniCG <60164761+LibniCG@users.noreply.github.com>
2020-08-30 16:22:48 -0500

    Add files via upload

--- orden.c
+++ orden.c
@@ -1,12 +1,12 @@
-#include <pthread-h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define NUM THREADS 2; /* Máximo de hilos que se pueden ocupar */
+#define NUM_THREADS 2 /* Máximo de hilos que se pueden ocupar */
 
 int *num; /* Variable para el arreglo */
-void *ordenamiento(void *param); /* the thread */
+void *ordenamiento(void *param); /* ordenamiento del arreglo */
 int comp(const void *a, const void *b);
 int num_cpus(); /* Información de la máquina */
 
@@ -17,38 +17,28 @@ int main(){
     clock_t tInicio, tFinal;
     double seg;
 
-    int lim, numHilos, numHilosMaquina;
+    int lim, numHilos;
     int lim1[2], lim2[2];
-    int i;
-    int bandera = 0; 
+    int i, bandera = 0; 
 
     /* Obtener largo del arreglo */
-    printf("Largo del arreglo");
+    printf("\n Largo del arreglo");
     scanf("%d", &lim);
 
     /* Obtener el número de hilos que tiene la máquina */
-    numHilosMaquina = num_cpus();
-
-    /* Tomar el tiempo de ejecución */
-
-    //////////////// A partir de donde ///////////
-    /*
-    tInicio = clock();
-    tFinal();
-    seg = (double)(tFinal - tInicio) / CLOCKS_PER_SEC;
-    printf("\n\n Se ha tardado: %f milisegundos en ejecutarse. \n", seg);
-    */
+    num_cpus();
 
     /* Validación para número de hilos */
     do {
         /* El usr indica el número de hilos a ocupar */
         printf("\n Número de hilos a ocupar: ");
         scanf("%d", &numHilos);
-        if(numHilos > 3 && numHilos == 0){
+        bandera = 0;
+        if(numHilos > 2 || numHilos == 0){
             bandera = 1;
-            printf(" Error, intente de nuevo.\n");
+            printf("\n Error, intente de nuevo.\n");
         }
-    } while(bandera = 1);
+    } while(bandera == 1);
 
     if(bandera == 0) {
         /* Reservación de memoria para el arreglo */
@@ -73,37 +63,45 @@ int main(){
         /* Validar número de hilos */
         lim1[0] = 0;
         if( numHilos == 1) {
-            lim1[1] = lim + 1;
-            pthread_create(workers[0], &attr, ordenamiento, &lim1);     
+            tInicio = clock();
+            lim1[1] = lim;
+
+            /* Creación del hilo */
+            pthread_create(&workers[0], &attr, ordenamiento, &lim1);
         } else { 
+            tInicio = clock();
             /* Definición de limites */
             lim1[1] = lim/2;
             lim2[0] = lim/2 + 1;
             lim2[1] = lim + 1;
 
             /* Creación de hilos */
-            pthread_create(workers[0], &attr, ordenamiento, &lim1);
-            pthread_create(workers[1], &attr, ordenamiento, &lim2);
+            pthread_create(&workers[0], &attr, ordenamiento, &lim1);
+            pthread_create(&workers[1], &attr, ordenamiento, &lim2);
         } 
 
         /* now wait for the thread to exit */
-        pthread_join(workers[0],NULL);
-        if( numHilos == 2) {
-            for(i = 0; i <= 2; i++){
-                pthread_join(workers[i],NULL);
-            }
-        } 
-        qsort(&num[lim1[0]], lim + 1, sizeof(int), comp); 
+        for(i = 0; i < numHilos; i++){
+            pthread_join(workers[i],NULL);
+        }
 
+        /* Ordenamiento del arreglo final */ 
+        qsort(&num[lim1[0]], lim + 1, sizeof(int), comp); 
+        tFinal = clock();
+        
         printf(" \n Arreglo ordenado: ");
         for(i = 0; i <= lim; i++){
-            num[i]  = random() % 100;
             if(i < lim){
                 printf(" %d, ", num[i]);
             } else {
-                printf(" %d ", num[i]);
+                printf(" %d \n\n", num[i]);
             }
         }
+
+        seg = (double)(tFinal - tInicio) / CLOCKS_PER_SEC;
+        printf(" Se ha tardado: %f milisegundos en ejecutarse con %d hilo(s). \n\n", seg, numHilos);
+
+        free(num);
     }
 }
 
@@ -118,7 +116,7 @@ void *ordenamiento(void *param) {
 }
 
 int comp(const void *a, const void *b) {
-    return(*int(*)a - *(int*)b);
+    return(*(int*)a - *(int*)b);
 }
 
 int num_cpus(){
@@ -133,6 +131,5 @@ int num_cpus(){
         : "0" (eax), "2" (ecx)
         : ) ;
         printf("\n Hilos disponibles: %d\n", ebx);
-    )
     return eax*ebx;
 }
